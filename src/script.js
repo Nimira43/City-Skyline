548bd7bc823a7a670054ea1d90ff4dc0676f206c
Nimira43 <n.rabone72@aol.com>
2024-12-17 15:48:38 +0000

    Setup basic scene

---
+++ src/script.js
@@ -0,0 +1,45 @@
+import * as THREE from 'three';
+
+const number = 20;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xeeeeee);
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 10, 20);
+camera.lookAt(0, 0, 0);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshLambertMaterial({ color: 0x009a17 });
+
+for (let i = 0; i < number; i++) {
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.set(Math.random() * 20 - 10, 0, Math.random() * 20 - 10);
+  scene.add(mesh);
+}
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(10, 10, 10);
+scene.add(directionalLight);
+
+renderer.setAnimationLoop(drawFrame);
+
+function drawFrame() {
+  scene.rotation.y += 0.005;
+  renderer.render(scene, camera);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
