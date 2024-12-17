fa8ec2696b0abae220d1989f0899f332298be2d5
Nimira43 <n.rabone72@aol.com>
2024-12-17 16:22:40 +0000

    Tweaking things around

--- src/script.js
+++ src/script.js
@@ -1,40 +1,40 @@
 import * as THREE from 'three'
 
-const number = 120;
+const number = 120
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.shadowMap.enabled = true
+renderer.shadowMap.enabled = true 
 document.body.appendChild(renderer.domElement)
 
 const scene = new THREE.Scene()
-scene.background = new THREE.Color(0x00b5e2)
+scene.background = new THREE.Color(0x333333)
 
 const aspect = window.innerWidth / window.innerHeight
 const camera = new THREE.PerspectiveCamera(30, aspect)
 camera.position.set(6, 7, 20)
 camera.lookAt(new THREE.Vector3(0, 0, 0))
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshLambertMaterial({ color: 0xdddddd })
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshPhongMaterial({ color: 0x555555 })
 
 for (let i = 0; i < number; i++) {
-  const height = Math.random() * 5 + 1
-  const building = new THREE.Mesh(geometry, material)
-  building.position.set(Math.random() * 20 - 10, 0, Math.random() * 20 - 10)
+  const height = Math.random() * 5 + 1;
+  const building = new THREE.Mesh(new THREE.BoxGeometry(1, height, 1), material)
+  building.position.set(Math.random() * 20 - 10, height / 2, Math.random() * 20 - 10)
   building.castShadow = true
   building.receiveShadow = true
   scene.add(building)
 }
 
-const light1 = new THREE.PointLight(0xffffff, 1)
+const light1 = new THREE.PointLight(0xffffff, 1);
 light1.position.set(3, 7, 9)
 light1.castShadow = true
 scene.add(light1)
 
 const light2 = new THREE.PointLight(0xffffff, 1)
 light2.position.set(3, 7, -9)
-light1.castShadow = true
+light2.castShadow = true
 scene.add(light2)
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
@@ -51,6 +51,7 @@ window.addEventListener('resize', onWindowResize, false)
 
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
+  camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight)
 }
+
