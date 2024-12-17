f2c09316b2709758489a88f47c03654258a31377
Nimira43 <n.rabone72@aol.com>
2024-12-17 16:36:26 +0000

    Used Directional lighting and made the buildings more reflective

--- src/script.js
+++ src/script.js
@@ -8,36 +8,39 @@ renderer.shadowMap.enabled = true
 document.body.appendChild(renderer.domElement)
 
 const scene = new THREE.Scene()
-scene.background = new THREE.Color(0x333333)
+scene.background = new THREE.Color(0xeeeeee)
 
 const aspect = window.innerWidth / window.innerHeight
 const camera = new THREE.PerspectiveCamera(30, aspect)
 camera.position.set(6, 7, 20)
 camera.lookAt(new THREE.Vector3(0, 0, 0))
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshPhongMaterial({ color: 0x555555 })
-
 for (let i = 0; i < number; i++) {
   const height = Math.random() * 5 + 1;
-  const building = new THREE.Mesh(new THREE.BoxGeometry(1, height, 1), material)
-  building.position.set(Math.random() * 20 - 10, height / 2, Math.random() * 20 - 10)
+  const geometry = new THREE.BoxGeometry(1, height, 1) 
+  const material = new THREE.MeshStandardMaterial({ 
+    color: 0x999999, 
+    roughness: 0.5, 
+    metalness: 0.5 
+  })
+  const building = new THREE.Mesh(geometry, material) 
+  building.position.set(Math.random() * 20 - 10, height / 2, Math.random() * 20 - 10) 
   building.castShadow = true
-  building.receiveShadow = true
+  building.receiveShadow = true 
   scene.add(building)
 }
 
-const light1 = new THREE.PointLight(0xffffff, 1);
-light1.position.set(3, 7, 9)
-light1.castShadow = true
-scene.add(light1)
+const directionalLight1 = new THREE.DirectionalLight(0xffffff, 1) 
+directionalLight1.position.set(10, 10, 10)
+directionalLight1.castShadow = true 
+scene.add(directionalLight1) 
 
-const light2 = new THREE.PointLight(0xffffff, 1)
-light2.position.set(3, 7, -9)
-light2.castShadow = true
-scene.add(light2)
+const directionalLight2 = new THREE.DirectionalLight(0xffffff, 1)
+directionalLight2.position.set(-10, 10, -10)
+directionalLight2.castShadow = true
+scene.add(directionalLight2)
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3)
 scene.add(ambientLight)
 
 renderer.setAnimationLoop(drawFrame)
